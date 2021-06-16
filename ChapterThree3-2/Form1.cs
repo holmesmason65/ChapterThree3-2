8e65d02b89f8b070ee66053f2e5463afb0a35c4e
holmesmason65 <mholmes022726@students.iticollege.edu>
2021-06-16 16:04:01 -0500

    Add project files.

---
+++ ChapterThree3-2/Form1.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Mason Holmes
+ * 6/16/2021
+ * This program provides a gui for navigating a database.
+ * 
+ * outputs: - titleTextBox : TextBox, displays book title 
+ *          - year_PublishedTextBox : TextBox, displays year book was published
+ *          - iSBNTextBox : TextBox, displays book ISBN number
+ *          - pubIDTextBox : TextBox, displays book publisher ID
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChapterThree3_2
+{
+    public partial class frmTitles : Form
+    {
+        public frmTitles()
+        {
+            InitializeComponent();
+        }
+
+        private void titlesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.titlesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.sQLBooksDBDataSet);
+
+        }
+
+        private void frmTitles_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'sQLBooksDBDataSet.Titles' table. You can move, or remove it, as needed.
+            this.titlesTableAdapter.Fill(this.sQLBooksDBDataSet.Titles);
+
+        }
+
+        private void titleTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
